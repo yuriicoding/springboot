71c49a88f678dfb206e2b28764002c9c6ed2ed78
yuriicoding <itdevelopment2021@gmail.com>
2023-05-29 19:02:01 -0700

    init push

---
+++ src/main/java/com/cmpt276/first_project/FirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cmpt276.first_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectApplication.class, args);
+	}
+
+}
